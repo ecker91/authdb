a316678686b0e589baadd922faeb8e672062e524
FERNANDO AGOSTINI ECKER <fernando_ecker@estudante.sesisenai.org.br>
2025-11-24 20:00:17 -0300

    correçao migrations e dockerfile do front

--- backend/prisma/migrations/20251113223108_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `valor` on the `pedido` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `pedido` DROP COLUMN `valor`;
--- backend/prisma/migrations/20251121010346_add_quantidade_to_produtos_em_pedidos/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `produtosempedidos` ADD COLUMN `quantidade` INTEGER NOT NULL DEFAULT 1;
